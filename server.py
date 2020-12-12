ef46fe90a4f0fe1106076d55decd27d2b2d0da35
x18zhan2 <hingjyutcheung@hotmail.com>
2020-12-12 12:33:50 +0100

    add some commits

--- server.py
+++ server.py
@@ -18,6 +18,7 @@ def on_new_client_request(ch, method, props, body):
 
     print("Got a new client request")
 
+    # once server gets a request, it generates a random uuid for its client
     new_client_uuid = str(uuid.uuid4())
     response = json.dumps({
         'client-uuid': new_client_uuid,
@@ -25,6 +26,7 @@ def on_new_client_request(ch, method, props, body):
         'latest-version-hash': latest_version_hash
     })
 
+    # server sends a response to client
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
                      properties=pika.BasicProperties(correlation_id = \
@@ -33,6 +35,7 @@ def on_new_client_request(ch, method, props, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
     print("Just added new client {}".format(new_client_uuid))
 
+# waiting for the first rpc request 
 def listen_for_new_clients(software_name):
     connection = pika.BlockingConnection()
     channel = connection.channel()
@@ -59,6 +62,7 @@ def publish_updates(software_name):
                              exchange_type='fanout')
     nb_updates = 0
 
+    # keep updating user's input
     while True:
         new_update_input = input("Do you want to publish an update?")
 
@@ -88,9 +92,11 @@ def publish_updates(software_name):
 
     connection.close()
 
+# start a thread to listen to new coming client
 new_clients_thread = Thread(target=listen_for_new_clients, args=[software_name])
 new_clients_thread.start()
 
+# in this thread, keep listening if user wants to publish new updates, 
 publish_updates(software_name)
 
 new_clients_thread.join(1.0)
